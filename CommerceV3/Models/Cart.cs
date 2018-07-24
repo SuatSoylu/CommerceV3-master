e4b9bf329b082fc9ab11d165bd0f70a42c4d0e7f
SUATHAKAN SOYLU <suatsoylu55@hotmail.com>
2018-07-24 15:38:02 +0300

    Add project files.

---
+++ CommerceV3/Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommerceV3.Models
+{
+    public class Cart
+    {
+		public Cart()
+		{
+			IsActive = true;
+		}
+		public String Id { get; set; }
+        [Display(Name = "Sahip: ")]
+        public String Owner { get; set; }
+		public virtual ICollection<CartItem> CardItems { get; set; }
+        [Display(Name = "Oluşturan Kullanıcı : ")]
+        public String CreatedBy { get; set; }
+        [Display(Name= "Oluşturma Tarihi")]
+		public DateTime CreateDate { get; set; }
+		public String UpdatedBy { get; set; }
+		public DateTime UpdateDate { get; set; }
+
+		public bool IsActive { get; set; }
+	}
+}
---
+++ CommerceV3/Models/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommerceV3.Models
+{
+    public class MenuItem
+    {
+		public String Id { get; set; }
+		[Display(Name = "Ad: ")]
+		public String Name { get; set; }
+		[Display(Name = "Bağlantı: ")]
+		public String Url { get; set; }
+		[Display(Name = "Hedef: ")]
+		public String Target { get; set; }
+		[Display(Name = "Yayında mı ?: ")]
+		public bool IsPublished { get; set; }
+		[Display(Name = "Pozisyon: ")]
+		public int Position { get; set; }
+
+		public string ParentMenuItemId { get; set; }
+		[ForeignKey("ParentMenuItemId")]
+		public MenuItem ParentMenuItem { get; set; }
+		public virtual ICollection<MenuItem> ChildMenuItems { get; set; }
+
+	}
+}
